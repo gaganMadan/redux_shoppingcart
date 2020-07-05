3c472a3f3a3ccbe6cd981769579e061dc02dd73c
gaganMadan <gagan7535@gmail.com>
2020-07-05 19:51:39 +0530

    2 files modified

--- reducer.js
+++ reducer.js
@@ -4,6 +4,10 @@ const Reducer = (state, actions) => {
     if(actions.type === CLEAR_CART){
         return {...state, cart : []}
     }
+    if(actions.type === REMOVE) {
+       return { ...state, cart : state.cart.filter((item) => item.id !== actions.payload.id)}
+      //console.log(actions.payload.id)
+    }
 
     return state
 }
